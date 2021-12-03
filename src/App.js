e4e8bfa99956c46f0ac35914e237d003b36ce3b8
katharinaweb <92152458+katharinaweb@users.noreply.github.com>
2021-12-03 19:47:08 +0100

    Created footer and wrapped everything in container

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import dictionary from "./dictionary.jpg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={dictionary}
-          className="img-fluid App-image"
-          alt="dictionary"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={dictionary}
+            className="img-fluid App-image"
+            alt="dictionary"
+          />
+        </header>
+        <footer className="App-footer">
+          This project was coded by Katharina Weberberger and is{" "}
+          <a
+            href="https://github.com/katharinaweb/react-dictionary-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://reverent-roentgen-62d65d.netlify.app/"
+            target="blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
