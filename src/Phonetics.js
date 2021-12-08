f18ffc8840e1cb527d6ce1a575262f4c2e500c47
katharinaweb <92152458+katharinaweb@users.noreply.github.com>
2021-12-08 19:21:25 +0100

    Eliminated warning message

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  console.log(props.phonetics);
   return (
     <div className="Phonetics">
       <a
